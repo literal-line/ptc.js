c8148b98c13c06950a50659213d135b72eed2f0e
Literal Line <13988479+shelbywallie2@users.noreply.github.com>
2020-07-09 22:25:08 -0400

    commands, new command directory, other stuff
    commands, command directory page, id/var name changes, color palette, herobrine

--- ptc.js
+++ ptc.js
@@ -1,47 +1,152 @@
-// ptc.js interpreter
-console.log('PetitComputer ver2.2');
-
+// ptc.js """"""interpreter""""""" by Literal Line
 document.getElementById('editScreen').style.display = 'none';
 
 
 
-var runScreen = (function() {
+var runMode = (function() {
 
+    // getElementById but shorter (this is bad??)
     var findId = function(id) {
         return document.getElementById(id);
     }
 
+    // set default canvas resolutions, all being 1024x768 except graphic canvas
+    var defaultRes = function() {
+        var list, index;
+
+        runGraphic.height = 192;
+        runGraphic.width = 256;
+
+        list = document.getElementsByClassName('runModeCanvas');
+        for (index = 0; index < list.length - 1; index++) {
+            list[index].setAttribute('height', 768);
+            list[index].setAttribute('width', 1024);
+        }
+    }
+
+
+    // define variables for canvases and set default resolutions
     var runConsole = findId('console');
     var runBGF = findId('bgFront');
     var runSprite = findId('sprite');
-    var runBGR = findId('bgR');
+    var runBGR = findId('bgRear');
     var runGraphic = findId('graphic');
+    defaultRes();
+
+    // set canvas contexts (this looks bad but oh well)
     var ctxCons, ctxBGF, ctxSprite, ctxBGR, ctxGraphic;
-
-    runConsole.height = 768;
-    runConsole.width = 1024;
     ctxCons = runConsole.getContext('2d');
+    ctxBGF = runBGF.getContext('2d');
+    ctxSprite = runSprite.getContext('2d');
+    ctxBGR = runBGR.getContext('2d');
+    ctxGraphic = runGraphic.getContext('2d');
 
-    console.log('runScreenController loaded');
+    // set default variables
+    var consolePallete = [ // regular color pallete for console, bg, and sprites
+        '#FFFFFF',
+        '#000000',
+        '#BFBFBF',
+        '#FFE000',
+        '#00F01F',
+        '#007F00',
+        '#FFCBA7',
+        '#FFA000',
+        '#975E2E',
+        '#00BFFF',
+        '#7F3FFF',
+        '#003FF0',
+        '#FF5FC0',
+        '#FF1F00',
+        '#3F3F3F',
+        '#EBEBEB'
+    ];
+    var consoleColor = consolePallete[0];
+    var consolePos = {
+        x: 0,
+        y: 0
+    };
 
 
-    return {
+    console.log('runMode controller loaded');
+
+
+    return { // all commands that can be used in run mode, categorized by their layer
 
         console: {
 
-            placeText: function(text, x, y) {
-                ctxCons.font = '48pt petitcomputer';
-                ctxCons.fillStyle = '#000000';
-                ctxCons.fillRect(x * 32, y * 32, text.length * 32, 32);
-                ctxCons.fillStyle = '#FFFFFF';
-                ctxCons.fillText(text, x * 32, (y + 1) * 32);
+            cls: function() {
+                ctxCons.clearRect(0, 0, 1024, 768);
+                consolePos.x = 0;
+                consolePos.y = 0;
             },
-            default: function() {
-                this.placeText('PetitComputer ver2.2', 0, 0);
-                this.placeText('SMILEBASIC 1048576 bytes free', 0, 1);
-                this.placeText('(C)2011-2012 SmileBoom Co.Ltd.', 0, 2);
-                this.placeText('READY', 0, 4);
-                this.placeText('(not really this is a test)', 0, 6);
+
+            locate: function(x, y) {
+                consolePos.x = x;
+                consolePos.y = y;
+            },
+
+            color: function(color) {
+                consoleColor = consolePallete[color];
+            },
+
+            print: function(text) { // print text on console layer with at x,y and color from palette
+                var x, y;
+                x = consolePos.x;
+                y = consolePos.y;
+
+                ctxCons.clearRect(x * 32, y * 32, text.length * 32, 32);
+                ctxCons.font = '48pt petitcomputer';
+                ctxCons.fillStyle = consoleColor;
+                ctxCons.fillText(text, x * 32, (y + 1) * 32);
+
+                consolePos.y++;
+            },
+
+            default: function() { // default text on start
+                this.cls();
+                this.print('PetitComputer ver2.2');
+                this.print('SMILEBASIC 1048576 bytes free');
+                this.print('(C)2011-2012 SmileBoom Co.Ltd.');
+                this.print('');
+                this.print('READY');
+                this.print('');
+                this.color(13);
+                this.print('At the moment, commands can');
+                this.print('only be inputted using the');
+                this.color(3);
+                this.print('Javascript');
+                this.locate(11,8);
+                this.color(13);
+                this.print('console.');
+            }
+
+        },
+
+        bgFront: {
+            //
+        },
+
+        sprite: { // sprite will stay in between BGF and BGR for now...
+            //
+        },
+
+        bgRear: {
+            //
+        },
+
+        graphic: {
+
+            gcls: function() {
+                ctxGraphic.clearRect(0, 0, 256, 192);
+            },
+            
+            gline: function(x1, y1, x2, y2, color) {
+                ctxGraphic.strokeStyle = consolePallete[color];
+                ctxGraphic.lineWidth = 1;
+                ctxGraphic.beginPath();
+                ctxGraphic.moveTo(x1, y1);
+                ctxGraphic.lineTo(x2, y2);
+                ctxGraphic.stroke();
             }
 
         }
@@ -51,16 +156,17 @@ var runScreen = (function() {
 })();
 
 
-var pnlScreen = (function() {
+var pnlScreen = (function() { // does nothing yet
 
     var canvas = document.getElementById('pnlScreen');
     var ctx = canvas.getContext('2d');
     canvas.height = 768;
     canvas.width = 1024;
 
-    console.log('pnlScreenController loaded');
+    console.log('pnlScreen controller loaded');
 
 })();
 
 
-runScreen.console.default();
+
+runMode.console.default();
